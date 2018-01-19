05d40cf8c73c846d40d61937125f808d16818a11
Rezaalisina <soltani_138988@yahoo.com>
2018-01-19 12:14:07 -0500

    Fixed unit tests

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# 346_project1
+# 346_project
+first I wrote Linkedlist library for given interface with the several method.
+wrote Hw1 generic class for implementing on LinkedList containing  several
+method. Then Hw1Test for the LinkedList. for each method couple test.``        
--- src/Hw1Test.java
+++ src/Hw1Test.java
@@ -1,23 +1,21 @@
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-
+import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class Hw1Test {
 
-    @Test
+    @org.junit.Test
     public void testNext() {
-        Hw1<Integer> actual = new Hw1 <Integer>().next();
-        Hw1<Integer> expected=null;
-        assertEquals(expected , actual);
+        Hw1<Integer> actual = new Hw1<Integer>().next();
+        Hw1<Integer> expected = null;
+        assertEquals(expected, actual);
     }
 
-    @Test
+    @org.junit.Test
     public void lastemptylist() {
         //setup
-        Hw1<Integer> a= new Hw1<Integer> ();
+        Hw1<Integer> a = new Hw1<Integer>();
         Hw1<Integer> x;
         //execute
         x = a.last();
@@ -25,10 +23,10 @@ public class Hw1Test {
         assertNull(x.get());
     }
 
-    @Test
+    @org.junit.Test
     public void after() {
         //setup
-        Hw1<Integer> b= new Hw1<Integer> ();
+        Hw1<Integer> b = new Hw1<Integer>();
         Hw1<Integer> y = new Hw1<>();
         b.set(1);
         y.set(3);
@@ -39,23 +37,23 @@ public class Hw1Test {
 
     }
 
-    @Test
+    @org.junit.Test
     public void detach() {
         //setup
-        Hw1<Integer> c = new Hw1<Integer> ();
+        Hw1<Integer> c = new Hw1<Integer>();
         Hw1<Integer> d = new Hw1<>();
         c.set(1);
         d.set(2);
         c.setNext(d);
         //execute
-       Hw1<Integer> z = c.detach();
-       Integer actual = z.get();
-       Integer expected = 2;
+        Hw1<Integer> z = c.detach();
+        Integer actual = z.get();
+        Integer expected = 2;
         //assert
-        assertEquals(expected,actual);
+        assertEquals(expected, actual);
     }
 
-    @Test
+    @org.junit.Test
     public void get() {
         Hw1<Integer> d = new Hw1<>();
         d.set(1);
@@ -80,7 +78,7 @@ public class Hw1Test {
         a.set(1);
         b.set(3);
         a.setNext(b);
-        Integer actual= a.next().get();
+        Integer actual = a.next().get();
         Integer expected = 3;
         assertEquals(expected, actual);
 
@@ -94,7 +92,7 @@ public class Hw1Test {
         a.set(1);
         b.set(3);
         a.append(b);
-        Integer actual= a.next().get();
+        Integer actual = a.next().get();
         Integer expected = 3;
         assertEquals(expected, actual);
     }
@@ -102,7 +100,7 @@ public class Hw1Test {
     @Test
     public void insert() {
         Hw1<Integer> a = new Hw1<>();
-        Hw1<Integer> b = new Hw1<> ();
+        Hw1<Integer> b = new Hw1<>();
         a.set(1);
         b.set(2);
         a.insert(b);
